b46d090dc43bb4647be10aceeddaf38fa40b76a5
Andrei Sebastian <andrei.virlan@student.upt.ro>
2023-04-28 16:02:37 +0300

    Added: panel, frame, button

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,15 +1,41 @@
 package GUI;
 
+import Benchmark.PICalculatorNewtonRaphson;
+import Timing.Timing;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+import static Logging.TimeUnit.Sec;
+import static Logging.TimeUnit.toTimeUnit;
+import static java.lang.System.exit;
+
+public class GUI implements ActionListener {
+
+    private int count = 0;
+    private JFrame frame;
+    private JPanel panel;
+    private JButton run_bench_button;
+    private JLabel label;
+
     public GUI(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+
+        run_bench_button = new JButton("Calculate PI using NewtonRaphson");
+        run_bench_button.addActionListener(this);
+
+        panel = new JPanel();
+
+        label = new JLabel("Output: ");
 
         panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(run_bench_button);
+        panel.add(label);
+
+
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,5 +43,24 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
 
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        if(count == 1){
+            Timing clock = new Timing();
+            PICalculatorNewtonRaphson pi1 = new PICalculatorNewtonRaphson();
+            pi1.initialize(100);
+            clock.start();
+
+            pi1.run();
+
+            long time = clock.stop();
+
+            label.setText("Output: " + pi1.getResult() + "\n it took " + toTimeUnit(time, Sec));
+        }
+        else if(count == 2){
+            exit(1);
+        }
     }
 }
