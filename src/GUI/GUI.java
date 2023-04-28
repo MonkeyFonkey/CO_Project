5289d14bbaec7357504ffca4d631fa6a73013f3b
Andrei Virlan <andrei.virlan@student.upt.ro>
2023-04-28 15:21:05 +0300

    Added some components to the GUI class

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,4 +1,21 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+    public GUI(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Benchmark");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ import GUI.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        Timing clock = new Timing();
-        PICalculatorNewtonRaphson pi1 = new PICalculatorNewtonRaphson();
-        pi1.initialize(100);
-        pi1.run();
-        pi1.getResult();
+//        Timing clock = new Timing();
+//        PICalculatorNewtonRaphson pi1 = new PICalculatorNewtonRaphson();
+//        pi1.initialize(100);
+//        pi1.run();
+//        pi1.getResult();
+
+        new GUI();
 
     }
 }
